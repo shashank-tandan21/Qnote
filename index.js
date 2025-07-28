d491676e53ff16c8cbea540ed14a46f742d6f63c
shashank-tandan21 <tandanshashank21@gmail.com>
2025-07-29 03:32:38 +0530

    index.js add

--- index.js
+++ index.js
@@ -3,10 +3,30 @@
 //     textArea.style.height = 'auto';
 //     textArea.style.height = textArea.scrollheight + 'px';
 // })
+
+// ## this can be writtennin this format also :-
+// for (let textArea of textAreas)
+// for (let i = 0; i < textAreas.length; i++) {
+//     let textArea = textAreas[i];
+
 const textAreas = document.getElementsByClassName('note-textarea');
 for (let textArea of textAreas) {
     textArea.addEventListener('input', () => {
         textArea.style.height = 'auto';
         textArea.style.height = textArea.scrollHeight + 'px';
     });
+}
+const shareButton = document.getElementById('sharecontent');
+shareButton.addEventListener('click', handleShare);
+function handleShare() {
+    if (navigator.share) {
+        navigator.share({
+            title: "text",
+            url: window.location.href
+        })
+            .then(() => console.log("shared successfully"))
+            .catch((error) => console.error("share failed:", error))
+    } else {
+        alert("your browser does not support web share API");
+    }
 }
